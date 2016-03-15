b5b27622cd2ab6a532839104b501acbe260dbe8a
ty <poongbreak@gmail.com>
2016-03-15 18:40:07 +0900

    레이아웃 추가 (ScanActivity.xml) 리스트뷰 수정

--- app/src/main/java/com/example/bluetooth/le/DeviceScanActivity.java
+++ app/src/main/java/com/example/bluetooth/le/DeviceScanActivity.java
@@ -16,6 +16,7 @@
 
 package com.example.bluetooth.le;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
@@ -24,7 +25,9 @@ import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -32,6 +35,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -44,8 +48,9 @@ import java.util.UUID;
 /**
  * Activity for scanning and displaying available Bluetooth LE devices.
  */
-public class DeviceScanActivity extends ListActivity {
+public class DeviceScanActivity extends Activity implements AdapterView.OnItemClickListener{
     private LeDeviceListAdapter mLeDeviceListAdapter;
+    private ListView m_ListView;
     private BluetoothAdapter mBluetoothAdapter;
     private boolean mScanning;
     private Handler mHandler;
@@ -59,9 +64,39 @@ public class DeviceScanActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.scan_activity);
         getActionBar().setTitle(R.string.title_devices);
         mHandler = new Handler();
+        m_ListView = (ListView)findViewById(R.id.listView);
+        m_ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final BluetoothDevice device = mLeDeviceListAdapter.getDevice(position);
+                if (device == null) return;
+                final Intent intent = new Intent(DeviceScanActivity.this, DeviceControlActivity.class);
+                intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_NAME, device.getName());
+                intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
+                if (mScanning) {
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                    mScanning = false;
+                }
+                startActivity(intent);
+            }
+        });
 
+        ActionBar actionBar = getActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(0xFF36c8a7));
+
+/*
+        view = (ImageView) findViewById(R.id.productImage);
+
+        // animation_list.xml 를 ImageView 백그라운드에 셋팅한다
+        view.setBackgroundResource(R.drawable.anim);
+
+        // 이미지를 동작시키기위해  AnimationDrawable 객체를 가져온다.
+        frameAnimation = (AnimationDrawable) view.getBackground();
+
+*/
         // Use this check to determine whether BLE is supported on the device.  Then you can
         // selectively disable BLE-related features.
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
@@ -127,8 +162,10 @@ public class DeviceScanActivity extends ListActivity {
         }
 
         // Initializes list view adapter.
+
         mLeDeviceListAdapter = new LeDeviceListAdapter();
-        setListAdapter(mLeDeviceListAdapter);
+        m_ListView.setAdapter(mLeDeviceListAdapter);
+        //setListAdapter(mLeDeviceListAdapter);
         scanLeDevice(true);
     }
 
@@ -149,6 +186,8 @@ public class DeviceScanActivity extends ListActivity {
         mLeDeviceListAdapter.clear();
     }
 
+
+    /*
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         final BluetoothDevice device = mLeDeviceListAdapter.getDevice(position);
@@ -162,7 +201,7 @@ public class DeviceScanActivity extends ListActivity {
         }
         startActivity(intent);
     }
-
+*/
     private void scanLeDevice(final boolean enable) {
         if (enable) {
             // Stops scanning after a pre-defined scan period.
@@ -182,6 +221,11 @@ public class DeviceScanActivity extends ListActivity {
             mBluetoothAdapter.stopLeScan(mLeScanCallback);
         }
         invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 
     // Adapter for holding devices found through scanning.
@@ -236,6 +280,7 @@ public class DeviceScanActivity extends ListActivity {
             if (view == null) {
                 view = mInflator.inflate(R.layout.listitem_device, null);
                 viewHolder = new ViewHolder();
+                viewHolder.deviceImage = (ImageView) view.findViewById(R.id.device_image);
                 viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
                 viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
                 viewHolder.deviceRssi = (TextView) view.findViewById(R.id.device_rssi);
@@ -252,6 +297,13 @@ public class DeviceScanActivity extends ListActivity {
             else
                 viewHolder.deviceName.setText(R.string.unknown_device);
 
+            // animation_list.xml 를 ImageView 백그라운드에 셋팅한다
+            //view.setBackgroundResource(R.drawable.anim);
+            //view.setBackgroundColor(Color.BLACK);
+
+            // 이미지를 동작시키기위해  AnimationDrawable 객체를 가져온다.
+            //frameAnimation = (AnimationDrawable) view.getBackground();
+            //frameAnimation.start();
             viewHolder.deviceAddress.setText(device.getAddress());
 
             viewHolder.deviceRssi.setText(String.valueOf(info.rssi)+"dBm");
@@ -280,6 +332,7 @@ public class DeviceScanActivity extends ListActivity {
         TextView deviceName;
         TextView deviceAddress;
         TextView deviceRssi;
+        ImageView deviceImage;
     }
 
     private class ScanInfo {
