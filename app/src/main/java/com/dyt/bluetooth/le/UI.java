8279f305dfb2ca7b2f491e2480558f2db9a25326
ty <poongbreak@gmail.com>
2016-03-31 13:59:42 +0900

    aa

--- app/src/main/java/com/dyt/bluetooth/le/UI.java
+++ app/src/main/java/com/dyt/bluetooth/le/UI.java
@@ -13,8 +13,13 @@ import android.widget.TextView;
 public class UI {
 
     private LinearLayout topLL;
+    private Context context;
 
-    protected void makeTitleView(Context context ,String str) {
+    public UI(Context context){
+        this.context = context;
+    }
+
+    protected void makeTitleView(String str) {
         LinearLayout.LayoutParams titleParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         LinearLayout titleLayout = new LinearLayout(context);
         TextView title = new TextView(context);
@@ -29,7 +34,7 @@ public class UI {
         titleLayout.addView(title);
     }
 
-    protected void makeDetailView(Context context, String str1, String str2) {
+    protected void makeDetailView(String str1, String str2) {
 
         LinearLayout.LayoutParams titleParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         titleParam.gravity = Gravity.CENTER;
@@ -37,29 +42,27 @@ public class UI {
         detailLayout.setOrientation(LinearLayout.HORIZONTAL);
         TextView detailTv = new TextView(context);
         LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        LinearLayout.LayoutParams param2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         detailTv.setBackgroundColor(Color.parseColor("#FFFFFF"));
         detailTv.setTextColor(Color.parseColor("#000000"));
         detailTv.setTextSize(13);
         detailTv.setText(str1);
         detailTv.setGravity(Gravity.CENTER);
 
-        param2.weight = 1;
-        detailTv.setLayoutParams(param2);
+        param.weight = 1;
+        detailTv.setLayoutParams(param);
         Button detailBtn = new Button(context);
         detailBtn.setBackgroundColor(Color.parseColor("#EEEEEE"));
         detailBtn.setTextColor(Color.parseColor("#000000"));
         detailBtn.setTextSize(13);
         detailBtn.setText(str2);
         detailBtn.setGravity(Gravity.CENTER);
-
-        param2.weight = 1;
-        detailBtn.setLayoutParams(param2);
+        param.weight = 1;
+        detailBtn.setLayoutParams(param);
 
         topLL.addView(detailLayout);
         detailLayout.addView(detailTv);
         detailLayout.addView(detailBtn);
-
+        //¤±¤± zzaas
+        //dfdf
     }
-
 }
